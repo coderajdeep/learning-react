1b0c6a59ec46d1eb86f7b99f95bba6663c3c5f67
coderajdeep <coder.rajdeep@gmail.com>
2023-11-13 20:13:38 +0530

    Initial react app complete

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const heading = React.createElement("h1", {id : "heading", xyz: "abc"}, "Hello World");
-// const paragraph = React.createElement("p", {}, "Created by React");
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(heading)
-root.render(heading)
-// root.render(paragraph)
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1", {id: "heading"}, "Hello World!");
+const paragraphOne = React.createElement("p", {id: "p1"}, "Paragraph one");
+const paragraphTwo = React.createElement("p", {id: "p2"}, "Paragraph two");
+const divElements = React.createElement("div", {id: "divIds"}, [paragraphOne, paragraphTwo]);
+const divs = React.createElement("div", {id: "divId"}, [heading, divElements]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(divs)
+root.render(divs)
