84a61b9e6c9cbc5e14bd351acb1f30cf57619c73
coderajdeep <coder.rajdeep@gmail.com>
2023-11-20 20:19:31 +0530

    Use React State

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,6 +12,15 @@ const Body = () => {
             </div>
             <div className="res-container">
                 {
+                    // Using IIFE
+                    // (() => {
+                    //     const ans = []
+                    //     for(let restaurant of restaurants) {
+                    //         ans.push(<RestaurantCard resObj = {restaurant.info} key={restaurant.info.id}/>) 
+                    //     }
+                    //     return ans
+                    // })()
+                    // Using map is generally used
                     restaurants.map(restaurant => <RestaurantCard resObj = {restaurant.info} key={restaurant.info.id}/>)
                 }
             </div>
