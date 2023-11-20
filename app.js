4b68e20fe1e7fef5d09b84488b10e44805c87e98
coderajdeep <coder.rajdeep@gmail.com>
2023-11-20 09:51:35 +0530

    Header completed

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-import React from "react";
 import ReactDOM from "react-dom/client"
 
-const headingJSX = <h1>HTML like syntex</h1>
-const paragraphOne = React.createElement("p", { id: "p1" }, "Paragraph One")
-const paragraphTwo = React.createElement("p", { id: "p2" }, "Paragraph Two")
-const divs = React.createElement("div", { id: "paragraphs" }, [paragraphOne, paragraphTwo])
-const heading = React.createElement("h1", { id: "heading" }, "Learning React")
-const pages = (<div>
-    <h1>JSX Heading</h1>
-    <p><i>JSX</i> Paragraph</p>
-</div>)
-const page = React.createElement("div", { id: "page" }, [heading, headingJSX, divs, pages])
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
 
-console.log(headingJSX, heading)
-root.render(page)
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://etimg.etb2bimg.com/photo/63298781.cms" alt="logo" className="logo"/>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
