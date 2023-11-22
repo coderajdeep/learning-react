9e4fec8dcf9c68a60ebb9502b3ded45e2f53e62d
coderajdeep <coder.rajdeep@gmail.com>
2023-11-22 19:07:55 +0530

    Move AppLayout into saperate file

---
+++ src/components/AppLayout.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Body from "./Body";
+
+const AppLayout = () => {
+    console.log('AppLayout');
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+};
+
+export default AppLayout
